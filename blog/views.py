a545a5dd6d7408c97fe30f07384e61be92b79e5b
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-07 22:14:31 +0300

    контроллеры и маршруты

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Создавайте свои представления здесь.
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def test(request):
+    return HttpResponse('<h1>Hola amigo!</h1>')
