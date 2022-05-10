76f405da22af5e9a086bc0c6f69ace47f627c254
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-11 00:48:10 +0300

    Шаблонизатор

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Blog
 # Создавайте свои представления здесь.
 
+
 def index(request):
-    return HttpResponse('Hello World')
+    blog = Blog.objects.order_by('-created_ad')
+    return render(request, 'blog/index.html', {'blog': blog, 'title': 'Список записей'})
