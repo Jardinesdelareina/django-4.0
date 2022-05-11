b511cd9f24e0d8f9c40fde6b1f1435a8feb501b0
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-12 00:06:34 +0300

    Связи моделей

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Category
 # Зарегистрируйте свои модели здесь.
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_ad', 'updated_ad', 'is_published')
+    list_display = ('id', 'category', 'title', 'created_ad', 'updated_ad', 'is_published')
     list_display_links = ('id', 'title')
+    list_editable = ('is_published',)
+    list_filter = ('category', 'is_published')
     search_fields = ('title', 'text')
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,16 +5,28 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField('Заголовок', max_length=50)
     text = models.TextField('Содержание', max_length=2000)
-    created_ad = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
-    updated_ad = models.DateTimeField(verbose_name='Обновлено', auto_now=True)
-    image = models.ImageField(verbose_name='Изображение', upload_to='image/%Y/%m/%d', blank=True)
-    is_published = models.BooleanField(verbose_name='Опубликовано', default=True)
+    created_ad = models.DateTimeField('Дата публикации', auto_now_add=True)
+    updated_ad = models.DateTimeField('Обновлено', auto_now=True)
+    image = models.ImageField('Изображение', upload_to='image/%Y/%m/%d', blank=True)
+    is_published = models.BooleanField('Опубликовано', default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
-
 
     class Meta:
         verbose_name = 'Запись'
         verbose_name_plural = 'Записи'
         ordering = ['-created_ad']
+
+
+class Category(models.Model):
+    title = models.CharField('Наименование категории', max_length=50, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
