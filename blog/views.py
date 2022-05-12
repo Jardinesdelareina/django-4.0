eb211d657cf36db26f1151b018cc922e3a352e36
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-12 21:59:22 +0300

    пользовательские теги шаблона

--- blog/views.py
+++ blog/views.py
@@ -7,22 +7,18 @@ from .models import Blog, Category
 
 def index(request):
     blog = Blog.objects.all()
-    categories = Category.objects.all()
     context = {
         'blog': blog, 
         'title': 'Список записей',
-        'categories': categories,
     }
     return render(request, 'blog/index.html', context)
 
 
 def get_category(request, category_id):
     blog = Blog.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'blog': blog,
-        'categories': categories,
         'category': category,
     }
     return render(request, 'blog/category.html', context)
