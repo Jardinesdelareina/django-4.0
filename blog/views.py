5600d0434cec25ed8f8905015f407d002a2cf36d
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-12 12:59:57 +0300

    параметры в url-запросах

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Blog
+from .models import Blog, Category
 # Создавайте свои представления здесь.
 
 
 def index(request):
     blog = Blog.objects.all()
-    return render(request, 'blog/index.html', {'blog': blog, 'title': 'Список записей'})
+    categories = Category.objects.all()
+    context = {
+        'blog': blog, 
+        'title': 'Список записей',
+        'categories': categories,
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def get_category(request, category_id):
+    blog = Blog.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'blog': blog,
+        'categories': categories,
+        'category': category,
+    }
+    return render(request, 'blog/category.html', context)
