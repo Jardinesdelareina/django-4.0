edb729ebce6e695745dd08737b44d44040f43651
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-14 02:07:31 +0300

    статические файлы

--- myapp/settings.py
+++ myapp/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 # Статические файлы (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp/static')
+]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Тип поля первичного ключа по умолчанию
