fa6e16bbbe63dc948e886ef84b27b314f4e2fb02
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-15 20:40:50 +0300

    классовые views

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('category/<int:category_id>/', get_category, name='category'),
+    path('', BlogView.as_view(), name='home'),
+    path('category/<int:category_id>/', BlogCategory.as_view(), name='category'),
     path('notes/<int:note_id>/', get_note, name='view_note'),
     path('notes/add_note/', add_note, name='add_note'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,41 @@
+from msilib.schema import ListView
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog, Category
 from .forms import BlogForm
+from django.views.generic import ListView
 
 # Создавайте свои представления здесь.
 
+class BlogView(ListView):
+    model = Blog
+    template_name = 'blog/index.html'
+    context_object_name = 'blog'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Все записи'
+        return context
+
+
+    def get_queryset(self):
+        return Blog.objects.filter(is_published=True)
+
+class BlogCategory(ListView):
+    model = Blog
+    template_name = 'blog/index.html'
+    context_object_name = 'blog'
+    allow_empty = False
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+
+    def get_queryset(self):
+        return Blog.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
 def index(request):
     blog = Blog.objects.all()
